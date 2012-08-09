1d270d2ca5c158a88e6e6c8479b2d447959ab6e3
Remco Tolsma <info@remcotolsma.nl>
2012-08-09 13:27:55 +0200

    WordPress start plugin files.

---
+++ pronamic-post-activation.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Pronamic Post Activation
+Plugin URI: http://pronamic.eu/wp-plugins/pronamic-post-activation/
+Description: The Pronamic Post Activation plugin allows you to activate and deactivate posts through an URL with an key.
+ 
+Version: 0.1
+Requires at least: 3.0
+
+Author: Pronamic
+Author URI: http://pronamic.eu/
+
+Text Domain: pronamic_post_activation
+Domain Path: /languages/
+
+License: GPL
+*/
+
+
